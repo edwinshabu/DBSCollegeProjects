c29e07a35c611a19d714561405d424d3d8712828
Edwin Shabu <edwinshabu1998@gmail.com>
2024-11-09 05:20:50 +0000

    Update app.py

--- HelloWorld/app.py
+++ HelloWorld/app.py
@@ -7,4 +7,4 @@ def hello_world():
     return "Hello, World!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=369)  # Update to match EXPOSE 369
+    app.run(host='0.0.0.0', port=80)  # Update to match EXPOSE 369
